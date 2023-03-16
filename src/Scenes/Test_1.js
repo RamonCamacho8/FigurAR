967d79990552de645bbd0c23443383d7ceb5f5c9
ArceCraft <118371477+RamonCamacho8@users.noreply.github.com>
2023-03-15 22:00:28 -0700

    commit

--- src/Scenes/Test_1.js
+++ src/Scenes/Test_1.js
@@ -1,37 +1,62 @@
-
 import React from "react";
 import * as BABYLON from "babylonjs";
-import * as MATERIALS from "babylonjs-materials"
+import * as MATERIALS from "babylonjs-materials";
 import SceneComponent from "../Babylon_components/SceneComponent";
 // import SceneComponent from 'babylonjs-hook'; // if you install 'babylonjs-hook' NPM.
 
-import * as Cameras_Module from "../Modules/Cameras_Module"
-import * as Lights_Module from "../Modules/Lights_Module"
+import * as Cameras_Module from "../Modules/Cameras_Module";
+import * as Lights_Module from "../Modules/Lights_Module";
 import * as Materials_Module from "../Modules/Materials_Module";
 
-import * as Babylon_Components from "../Babylon_components"
+import * as Babylon_Components from "../Babylon_components";
 
-import * as XR_Module from "../Modules/XR_Module"
-import * as Gizmo from "../Modules/GizmoInterface"
+import * as XR_Module from "../Modules/XR_Module";
+import * as Gizmo from "../Modules/GizmoInterface";
+import "babylonjs-loaders";
+import Pyramid from "../Models/Pyramid.glb";
 
+const onSceneReady = async (
+  e = {
+    engine: new BABYLON.Engine(),
+    scene: new BABYLON.Scene(),
+    canvas: new HTMLCanvasElement(),
+  }
+) => {
+  const { canvas, scene, engine } = e;
 
+  Cameras_Module.FreeCameraDefault(canvas, scene);
+  const camera = new BABYLON.FreeCamera(
+    "camera1",
+    new BABYLON.Vector3(0, 5, -10),
+    scene
+  );
+  Lights_Module.HemisphericLight(scene);
 
-
-const onSceneReady = async (e = { engine: new BABYLON.Engine, scene: new BABYLON.Scene, canvas: new HTMLCanvasElement }) => {
-
-    const { canvas, scene, engine } = e;
+  const pyramid = await BABYLON.SceneLoader.ImportMeshAsync(
+    "",
+    Pyramid,
+    "",
+    scene,
+    function (meshes) {
+        
+    }
+  );
   
-    Cameras_Module.FreeCameraDefault(canvas, scene);
-    const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-    Lights_Module.HemisphericLight(scene);
+  let pyramidMesh = pyramid.meshes[0];
+  const material = new BABYLON.StandardMaterial("material", scene);
+  material.diffuseColor = new BABYLON.Color3(0, 0, 1);
 
-    const result = await BABYLON.SceneLoader.ImportMeshAsync("", "../Models", "Pyramid.gltf", scene);
+  pyramidMesh.material = material;
 
-    camera.target = result.meshes[0];
 
-    console.log(result.meshes[0])
-  
-  };
+
+  const assetManager = new BABYLON.AssetsManager(scene);
+  assetManager.load();
+
+ 
+};
+
+
 
 function Scene() {
   return (
