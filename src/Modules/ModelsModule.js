55492b7ca647cb063064f1074bbcccba71c127d7
ArceCraft <118371477+RamonCamacho8@users.noreply.github.com>
2023-03-29 22:39:43 -0700

    Commit

--- src/Modules/ModelsModule.js
+++ src/Modules/ModelsModule.js
@@ -1,7 +1,9 @@
-import { SceneLoader } from "babylonjs";
+import { HemisphericLight, Scene, SceneLoader } from "babylonjs";
 import Figures from "../Models/Figures.glb";
 import * as BABYLON from "babylonjs";
 import earcut from "earcut";
+import * as GUI from "babylonjs-gui";
+import ammo from "ammo.js";
 
 export async function CreatePyramid(){
 
@@ -37,24 +39,213 @@ export async function CreateSphere(){
 export async function CreateWalls(){
     
         const {meshes} = await SceneLoader.ImportMeshAsync("Walls",Figures,"");
-    
+        meshes.map((mesh) => {
+            mesh.checkCollisions = true;
+        })
         return meshes[1];
 }
 
 export async function CreateGround(){
 
     const {meshes} = await SceneLoader.ImportMeshAsync("Ground",Figures,"");
-
+    meshes.map((mesh) => {
+        mesh.checkCollisions = true;
+    })
     return meshes[1];
+
+}
+
+export async function SetupScene(scene){
+    
+    new HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+
+    
+            
+
+    const frameRate = 60;
+    const gravity = -9.81;
+    scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.AmmoJSPlugin(true,await ammo()));
+    scene.gravity = new BABYLON.Vector3(0, -0.98, 0)
+    scene.collitionsEnabled = true;
 
 }
 
 export async function CreateBoard(){
 
     const {meshes} = await SceneLoader.ImportMeshAsync("Board",Figures,"");
-
     return meshes[1];
 
+}
+/**
+ *
+ * 
+ * @param {Scene} scene string name of the instanced material 
+ */
+export function CreateController(scene){
+    const camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 1.7, 0), scene);
+    camera.attachControl();
+    camera.checkCollisions = true;
+    camera.applyGravity = true;
+    camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+    camera.speed = 0.5;
+    camera.minZ = 0.45;
+    camera.angularSensibility = 4000;
+}
+
+/**
+ *
+ * @param {Mesh} mesh string name of the instanced material
+ * @param {Scene} scene string name of the instanced material 
+ * @param {string} panelText string name of the instanced material
+ * @param {boolean} imported string name of the instanced material
+ */
+export function CreateFloatingPanel(mesh,scene, panelText, imported){
+    // GUI
+  let meshGUI = BABYLON.MeshBuilder.CreatePlane(
+    "plane",
+    {
+      width: 1 * 1.8,
+      height: 1,
+      sideOrientation: BABYLON.Mesh.DOUBLESIDE,
+    },
+    scene
+  );
+
+  meshGUI.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+  let advancedTexture = GUI.AdvancedDynamicTexture.CreateForMesh(meshGUI);
+
+  let container = new GUI.Rectangle("container");
+  container.width = 1;
+  container.height = 1;
+  container.thickness = 0;
+  container.background = "white";
+  container.alpha = 0.05;
+  container.zIndex = -1;
+
+  advancedTexture.addControl(container);
+  advancedTexture.scaleTo(300, 150);
+
+  let button = GUI.Button.CreateSimpleButton("but1", panelText);
+  button.color = "white";
+  button.fontSize = 20;
+  button.background = "green";
+  button.onPointerEnterObservable.add(function () {
+    console.log("click");
+  });
+
+  advancedTexture.addControl(button);
+
+  meshGUI.parent =  mesh;
+  meshGUI.position = new BABYLON.Vector3(0, -2, 0);
+
+    if(imported){
+        
+        let angle = 90;
+        meshGUI.rotation = new BABYLON.Vector3(angle*Math.PI / 2, 0, 0);
+    }
+
+    return {meshGUI, advancedTexture, container, button};
+}
+
+export function CreateFloatingSlider(mesh, scene, panelText, imported) {
+  // GUI
+  let meshGUI = BABYLON.MeshBuilder.CreatePlane(
+    "plane",
+    {
+      width: 1 * 1.8,
+      height: 1,
+      sideOrientation: BABYLON.Mesh.DOUBLESIDE,
+    },
+    scene
+  );
+
+  meshGUI.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+  let advancedTexture = GUI.AdvancedDynamicTexture.CreateForMesh(meshGUI);
+
+  let grid = new GUI.Grid();
+  grid.addColumnDefinition(0.5);
+  grid.addColumnDefinition(0.5);
+  grid.addRowDefinition(0.5);
+
+  grid.widthInPixels = 500;
+  grid.heightInPixels = 500;
+  //grid.background = "black"; 
+
+  let panel = new GUI.StackPanel();
+  panel.width = "220px";
+  panel.isVertical = true;
+  /*panel.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+  panel.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;*/
+
+  let header = new GUI.TextBlock();
+  header.text = "Angulo: 90°";
+  header.height = "30px";
+  header.color = "white";
+
+  let slider = new GUI.Slider();
+  slider.minimum = 5;
+  slider.maximum = 175;
+  slider.value = 90;
+  slider.step = 1;
+  slider.height = "40px";
+  slider.width = "200px";
+
+  let information = new GUI.TextBlock();
+  information.text = "A: 90° \n B: 90° \n C: 90°";
+  information.height = "150px";
+  information.color = "white";
+
+  slider.onPointerEnterObservable.add(function () {
+    slider.isHighlighted = true;
+  });
+  slider.onPointerOutObservable.add(function () {
+    slider.isHighlighted = false;
+  });
+  slider.onValueChangedObservable.add(function (value) {
+    
+    value = parseInt(value);
+
+    let radius = 1;
+    let a = radius*2;
+    let x1 = radius;
+    let y1 = 0;
+    let x2 = -radius;
+    let y2 = 0;
+    let x3 = 1*Math.cos(value* Math.PI /180);
+    let y3 = 1*Math.sin(value* Math.PI /180);
+    
+    let b = Math.sqrt(Math.pow(x3-x1,2)+Math.pow(y3-y1,2));
+    let c = Math.sqrt(Math.pow(x3-x2,2)+Math.pow(y3-y2,2));
+
+    
+    header.text = "Angulo: " + value + "°";
+    information.text = "A: "+a.toPrecision(4)+" \n B: "+b.toPrecision(4)+" \n C: "+c.toPrecision(4)+"";
+
+    if (mesh) {
+      //cast to int
+      UpdateTriangleAngle(mesh, value);
+    }
+  });
+
+  
+
+  
+
+  advancedTexture.addControl(grid);
+  panel.addControl(header);
+  panel.addControl(slider);
+  grid.addControl(panel,0,0);
+  grid.addControl(information,0,1);
+
+  advancedTexture.scaleTo(500, 150);
+
+  meshGUI.parent = mesh;
+  meshGUI.position = new BABYLON.Vector3(0, -1, 0);
+
+  if (imported) {
+    let angle = 90;
+    meshGUI.rotation = new BABYLON.Vector3((angle * Math.PI) / 2, 0, 0);
+  }
 }
 
 
--- src/Scenes/Test_1.js
+++ src/Scenes/Test_1.js
@@ -19,41 +19,27 @@ const onSceneReady = async (
 ) => {
   const { canvas, scene, engine } = e;
 
-  // This creates and positions a free camera (non-mesh)
-  var camera = new BABYLON.FreeCamera(
-    "camera1",
-    new BABYLON.Vector3(0, 2, 0),
-    scene
-  );
+  scene.onPointerDown = (evt) => {
+    if (evt.button === 0) engine.enterPointerlock();
+    if (evt.button === 1) engine.exitPointerlock();
+  };
 
 
-  //const camera = new BABYLON.ArcRotateCamera("camera1", 0, 0, 0, new BABYLON.Vector3(2, 3, 4), scene);
+  // Setup scene
+  ModelsModule.SetupScene(scene);
 
-  // This targets the camera to scene origin
-  camera.setTarget(BABYLON.Vector3.Zero());
-  //camera.setPosition(new BABYLON.Vector3(10, 3, -10))
-
-  // This attaches the camera to the canvass
-  camera.attachControl(canvas, false);
-
-  //scene.clearColor = new BABYLON.Color3(0, 0, 0);
-
-  var light = new BABYLON.HemisphericLight(
-    "light",
-    new BABYLON.Vector3(0, 1, 0),
-    scene
-  );
-  light.intensity = 0.7;
+  // Camera
+  ModelsModule.CreateController(scene);
 
   // GUI
-
   GizmoInterface(scene);
   
-  var cylinder = await ModelsModule.CreateCylinder("cylinder", 4, scene,1,1);
-  var cube = await ModelsModule.CreateCube("cube", 4, scene,1,1);
-  var sphere = await ModelsModule.CreateSphere("sphere", 4, scene,1,1);
-  var pyramid = await ModelsModule.CreatePyramid("pyramid", 4, scene,1,1);
-  var board = await ModelsModule.CreateBoard("board", 4, scene,1,1);
+  var cylinder = await ModelsModule.CreateCylinder();
+  var cube = await ModelsModule.CreateCube();
+  var sphere = await ModelsModule.CreateSphere();
+  var pyramid = await ModelsModule.CreatePyramid();
+  var board = await ModelsModule.CreateBoard();
+
   ModelsModule.CreateWalls();
   ModelsModule.CreateGround();
   
@@ -61,123 +47,29 @@ const onSceneReady = async (
   scene.debugLayer.show();
 
   // GUI
-  var meshGUI = BABYLON.MeshBuilder.CreatePlane(
-    "plane",
-    {
-      width: 1 * 1.8,
-      height: 1,
-      sideOrientation: BABYLON.Mesh.DOUBLESIDE,
-    },
-    scene
-  );
-
-  meshGUI.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
-
-  var meshGUI_2 = BABYLON.MeshBuilder.CreatePlane(
-    "plane_2",
-    {
-      width: 1 * 1.8,
-      height: 1,
-      sideOrientation: BABYLON.Mesh.DOUBLESIDE,
-    },
-    scene
-  );
-
-  meshGUI_2.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
-
-  var advancedTexture = GUI.AdvancedDynamicTexture.CreateForMesh(meshGUI);
-  var advancedTexture_2 = GUI.AdvancedDynamicTexture.CreateForMesh(meshGUI_2);
-  
-
-  var container = new GUI.Rectangle("container");
-  container.width = 1;
-  container.height = 1;
-  container.thickness = 0;
-  container.background = "white";
-  container.alpha = 0.05;
-  container.zIndex = -1;
-
-  var container_2 = new GUI.Rectangle("container2");
-  container.width = 1;
-  container.height = 1;
-  container.thickness = 0;
-  container.background = "white";
-  container.alpha = 0.05;
-  container.zIndex = -1;
-
-  advancedTexture.addControl(container);
-  advancedTexture.scaleTo(300, 150);
-  advancedTexture_2.addControl(container_2);
-  advancedTexture_2.scaleTo(300, 150);
-
-
-  var button1 = GUI.Button.CreateSimpleButton("but1", "Cilindro\n Su volumen se calcula con la formula: \n V = π*radio^2*altura");
-  button1.color = "white";
-  button1.fontSize = 20;
-  button1.background = "green";
-  button1.onPointerUpObservable.add(function () {
-    alert("Lo hiciste!");
-  });
-
-
-  var button2 = GUI.Button.CreateSimpleButton("but2", "Cubo\n Todas sus caras son cuadradas y sus aristas son iguales. \n Su volumen se calcula con la formula: \n V = lado^3");
-  button2.color = "white";
-  button2.fontSize = 20;
-  button2.background = "green";
-  button2.onPointerUpObservable.add(function () {
-    alert("Lo hiciste!");
-  });
-
-  advancedTexture.addControl(button1);
-  advancedTexture_2.addControl(button2);
-
-  meshGUI_2.parent = cube;
-  meshGUI_2.position = new BABYLON.Vector3(0, -2, 0);
-  let angle_2 = 90;
-  meshGUI_2.rotation = new BABYLON.Vector3(angle_2*Math.PI / 2, 0, 0);
-
-  meshGUI.parent = await cylinder;
-  meshGUI.position = new BABYLON.Vector3(0, -2, 0);
-  let angle = 90;
-  meshGUI.rotation = new BABYLON.Vector3(angle*Math.PI / 2, 0, 0);
-
-  
- 
-	
-  
+  ModelsModule.CreateFloatingPanel(cylinder, scene, "Cylinder", true);
+  ModelsModule.CreateFloatingPanel(cube, scene, "Cube", true);
+  ModelsModule.CreateFloatingPanel(sphere, scene, "Sphere", true);
+  ModelsModule.CreateFloatingPanel(pyramid, scene, "Pyramid", true);
 
   var deltaTimeInSeconds = 0;
   var angleTriangle = 5;
   var sign = 1;
   var triangle = ModelsModule.CreateTriangleInsideSemiCircle("Triangle", angleTriangle, scene)
+
+  ModelsModule.CreateFloatingSlider(triangle, scene, "Pyramid", true);
+
   triangle.position = new BABYLON.Vector3(0, 0, -.5);
   triangle.material = new BABYLON.StandardMaterial("material", scene);
   triangle.material.diffuseColor = new BABYLON.Color3(0, 0, 1);
 
-  camera.target = board.position;
+  //camera.target = board.position;
   
   triangle.rotate(BABYLON.Axis.Y,90*Math.PI/180, BABYLON.Space.LOCAL);
   triangle.rotate(BABYLON.Axis.Z,90*Math.PI/180, BABYLON.Space.LOCAL);
-
- 
-
   triangle.parent = board;
+
   scene.onBeforeRenderObservable.add(() =>{
-    if (triangle !== undefined) {
-
-      deltaTimeInSeconds += scene.getEngine().getDeltaTime()/1000;
-      if(deltaTimeInSeconds >= .1){
-        ModelsModule.UpdateTriangleAngle(triangle, angleTriangle)
-        angleTriangle += 1*sign;  
-        deltaTimeInSeconds = 0;
-        if(angleTriangle >= 175 || angleTriangle <= 5)
-          sign = sign*(-1);
-
-      } 
-  
-       
-    } 
-
   });
 
   engine.runRenderLoop(() => {
